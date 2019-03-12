ff141513460be15d3e7b9fffddbd3eed93b07744
Dhruvin_prajapati <djprajapati1056@outlook.com>
2019-03-12 21:46:32 +0530

    set image in back ground and top font set

--- lib/ui/klimatic.dart
+++ lib/ui/klimatic.dart
@@ -14,9 +14,42 @@ class _KlimaticState extends State<Klimatic> {
     return new Scaffold(
       appBar: AppBar(
         title: new Text("Klimatic"),
-        backgroundColor: Colors.black,
-        actions: <Widget>[],
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            onPressed: null,
+          )
+        ],
+      ),
+      body: new Stack(
+        children: <Widget>[
+          new Center(
+            child: new Image.asset('images/umbrella.png',
+            width: 490.0,
+            height: 1200.0,
+            fit: BoxFit.fill,
+            ),
+
+          ),
+          new Container(
+            alignment: Alignment.topRight,
+            margin: const EdgeInsets.fromLTRB(0.0, 11, 21, 0),
+            child: new Text("Ahmedabad",
+              style: cityStyle(),
+            ),
+          )
+        ],
       ),
     );
   }
+}
+
+TextStyle cityStyle(){
+  return new TextStyle(
+    color: Colors.white,
+    fontSize: 23,
+    fontStyle: FontStyle.italic
+  );
 }
